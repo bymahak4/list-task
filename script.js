b59f673ee3995442579f46e83649e37ced61a874
bymahak4 <mahaka.uy@gmail.com>
2022-11-03 11:07:42 -0300

    task list base

---
+++ script.js
@@ -0,0 +1,54 @@
+const newTaskForm   = document.querySelector(".add-task-form");
+const addListTask   = document.querySelector(".list-task");
+const searchBar     = document.querySelector(".input-search-bar");
+
+//ADD
+const newTask = (e) => `<div class="task-container">
+    <p class="task-name">${e}</p>
+    <p class="task-item-delete">X</p>
+  </div>`
+;
+
+newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const lengthErrorMsg    = document.querySelector(".length-error");
+    const taskbox           = newTaskForm.addTask.value.trim();
+    
+
+    if (taskbox.length > 0) {
+        addListTask.innerHTML += newTask(taskbox);
+        newTaskForm.reset();
+        if (lengthErrorMsg != null) {
+            lengthErrorMsg.remove();
+        }
+    } else if (lengthErrorMsg === null) {
+        newTaskForm.innerHTML += `<p class="length-error">Ingrese una tarea</p>`;
+    }
+});
+
+
+//DEL
+addListTask.addEventListener("click", (e) => {
+    if (e.target.classList.contains("task-item-delete")) {
+        e.target.parentElement.remove();
+    }
+});
+
+
+//SEARCH
+const searchFunction = (input) => {
+    const taskNameContainer = document.querySelectorAll(".task-container");
+
+    Array.from(taskNameContainer).forEach((item) => {
+        if(!item.firstElementChild.innerHTML.toLowerCase().includes(input)) {
+            item.classList.add("filtered");
+        } else if (item.firstElementChild.innerHTML.toLowerCase().includes(input)) {
+            item.classList.remove("filtered");
+        }
+    });
+};
+
+searchBar.addEventListener("keyup", () => {
+    let input = searchBar.value.trim().toLocaleLowerCase();
+    searchFunction(input);
+});
